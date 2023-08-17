53ad764565914bc9ba7ae6ef1c3ebff863510933
ss5815190 <ss5815190@gmail.com>
2023-08-17 17:40:40 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Meals from './components/Meals';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Meals />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import classes from '../styles/Header.module.css';
+import mealsImage from '../assets/meals.jpg';
 
 function Header() {
   return (
-    <header>Header</header>
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </>
   );
 }
 
