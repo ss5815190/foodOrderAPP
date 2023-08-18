70a524f9ff63b9a5a6e18e065a12bc864eea6fdb
ss5815190 <ss5815190@gmail.com>
2023-08-18 17:02:14 +0800

    HeaderCartButton jump

--- src/components/HeaderCartButton.jsx
+++ src/components/HeaderCartButton.jsx
@@ -1,12 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import classes from '../styles/Header.module.css';
 import CartIcon from './UI/CartIcon';
 import { CartContext } from '../context/Context';
 
 function HeaderCartButton() {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const { totalQuantity } = useContext(CartContext);
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+  useEffect(() => {
+    if (totalQuantity === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    // eslint-disable-next-line
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [totalQuantity]);
   return (
-    <button type="button" className={classes.button} onClick={() => console.log('clicked')}>
+    <button type="button" className={btnClasses} onClick={() => console.log('clicked')}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
