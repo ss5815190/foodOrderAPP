501a52bd3b426b967745d880490871b59296ef00
ss5815190 <ss5815190@gmail.com>
2023-08-21 09:57:02 +0800

    change to useReducer

--- src/components/Meals/MealItem.jsx
+++ src/components/Meals/MealItem.jsx
@@ -7,44 +7,69 @@ function MealItem({
   id, name, description, price,
 }) {
   const amountInputRef = useRef();
-  const {
-    cart, setCart, totalQuantity, setTotalQuantity,
-  } = useContext(CartContext);
+  const { Cart, dispatch } = useContext(CartContext);
+  // const {
+  //   cart, setCart, totalQuantity, setTotalQuantity,
+  // } = useContext(CartContext);
   // 檢查購物車有沒有該商品
+  // const checkCart = (checkId, quantity) => {
+  //   if (CartContext.length !== 0) {
+  //     const index = cart.findIndex((item) => item.id === checkId);
+  //     // 找到的話更改數量
+  //     if (index !== -1) {
+  //       const updatedCart = [...cart];
+  //       // 傳進來的quantity是字串 轉成數字加上去
+  //       updatedCart[index].quantity += parseInt(quantity, 10);
+  //       setCart(updatedCart);
+  //       setTotalQuantity((totalQuantity + parseInt(quantity, 10)));
+  //     } else {
+  //       setCart((prev) => [...prev,
+  //         {
+  //           id: amountInputRef.current.id,
+  //           name,
+  //           description,
+  //           price,
+  //           quantity: 1,
+  //         }]);
+  //       setTotalQuantity((totalQuantity + parseInt(quantity, 10)));
+  //     }
+  //   } else {
+  //     setCart((prev) => [...prev,
+  //       {
+  //         id: amountInputRef.current.id,
+  //         name,
+  //         description,
+  //         price,
+  //         num: 1,
+  //       }]);
+  //     setTotalQuantity(totalQuantity + parseInt(quantity, 10));
+  //   }
+  // };
+  // 加入購物車
   const checkCart = (checkId, quantity) => {
-    if (CartContext.length !== 0) {
-      const index = cart.findIndex((item) => item.id === checkId);
-      // 找到的話更改數量
-      if (index !== -1) {
-        const updatedCart = [...cart];
-        // 傳進來的quantity是字串 轉成數字加上去
-        updatedCart[index].quantity += parseInt(quantity, 10);
-        setCart(updatedCart);
-        setTotalQuantity((totalQuantity + parseInt(quantity, 10)));
-      } else {
-        setCart((prev) => [...prev,
-          {
-            id: amountInputRef.current.id,
-            name,
-            description,
-            price,
-            quantity: 1,
-          }]);
-        setTotalQuantity((totalQuantity + parseInt(quantity, 10)));
-      }
+    const existingItemIndex = Cart.findIndex((item) => item.id === checkId);
+
+    if (existingItemIndex !== -1) {
+      // 增加數量
+      dispatch({
+        type: 'INCREMENT_QUANTITY',
+        payload: { id: checkId, quantity: parseInt(quantity, 10) },
+      });
     } else {
-      setCart((prev) => [...prev,
-        {
+      // 新增商品到購物車
+      dispatch({
+        type: 'ADD_TO_CART',
+        payload: {
           id: amountInputRef.current.id,
           name,
           description,
           price,
-          num: 1,
-        }]);
-      setTotalQuantity(totalQuantity + parseInt(quantity, 10));
+          quantity: parseInt(quantity, 10),
+        },
+      });
     }
   };
-  // 加入購物車
+
   const onSubmit = (e) => {
     e.preventDefault();
     checkCart(amountInputRef.current.id, amountInputRef.current.value);
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,10 +1,12 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, {
+  createContext, useState, useEffect, useReducer,
+} from 'react';
 
 export const CartContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export function CartContextProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  // const [cart, setCart] = useState([]);
   const [totalQuantity, setTotalQuantity] = useState(0);
   const [cartIsShown, setCartIsShown] = useState(false);
 
@@ -35,18 +37,39 @@ export function CartContextProvider({ children }) {
     },
   ];
   const [mealItem, setMealItem] = useState(DUMMY_MEALS);
-  useEffect(() => { console.log('Cart ', cart); }, [cart]);
+  // useEffect(() => { console.log('Cart ', cart); }, [cart]);
+
+  const cartReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TO_CART':
+        setTotalQuantity(totalQuantity + 1);
+        return [...state, action.payload];
+      case 'INCREMENT_QUANTITY':
+        return state.map((item) => {
+          if (item.id === action.payload.id) {
+            setTotalQuantity(totalQuantity + action.payload.quantity);
+            return { ...item, quantity: item.quantity + action.payload.quantity };
+          }
+          return item;
+        });
+      default:
+        return state;
+    }
+  };
+
+  const [Cart, dispatch] = useReducer(cartReducer, []);
+  useEffect(() => { console.log('Cart ', Cart); }, [Cart]);
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
     <CartContext.Provider value={{
-      cart,
-      setCart,
       mealItem,
       setMealItem,
       totalQuantity,
       setTotalQuantity,
       cartIsShown,
       setCartIsShown,
+      Cart,
+      dispatch,
     }}
     >
       {children}
