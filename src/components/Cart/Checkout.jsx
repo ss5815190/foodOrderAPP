4613553175487dc6cf84b30388982e7783d5d057
ss5815190 <ss5815190@gmail.com>
2023-08-24 15:07:47 +0800

    form validation

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useRef, useContext } from 'react';
+import React, { useRef, useContext, useState } from 'react';
 import classes from '../../styles/Checkout.module.css';
 import { CartContext } from '../../context/Context';
 
@@ -14,6 +14,12 @@ function Checkout({ onCancel, onSubmit, setIsSubmit }) {
   const streetInputRef = useRef();
   const postalCodeInputRef = useRef();
   const cityInputRef = useRef();
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
 
   const confirmHandler = (event) => {
     event.preventDefault();
@@ -22,12 +28,18 @@ function Checkout({ onCancel, onSubmit, setIsSubmit }) {
     const enteredPostalCode = postalCodeInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
     // 表單驗證
+    setFormInputsValidity({
+      name: !isEmpty(enteredName),
+      street: !isEmpty(enteredStreet),
+      city: !isEmpty(enteredCity),
+      postalCode: isFiveChars(enteredPostalCode),
+    });
+
     const formIsValid = !isEmpty(enteredName)
       && !isEmpty(enteredStreet)
       && !isEmpty(enteredCity)
       && isFiveChars(enteredPostalCode);
     if (!formIsValid) {
-      console.log('input error!');
       return null;
     }
     // 送出訂單
@@ -41,31 +53,39 @@ function Checkout({ onCancel, onSubmit, setIsSubmit }) {
     setIsSubmit(true);
     return null;
   };
+  // 輸入錯誤變紅
+  const nameControlClasses = ({ id }) => `${classes.control} ${
+    formInputsValidity[id] ? '' : classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameControlClasses({ id: 'name' })}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please enter a valid name!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={nameControlClasses({ id: 'street' })}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
+        {!formInputsValidity.street && <p>Please enter a valid street!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={nameControlClasses({ id: 'postalCode' })}>
         <label htmlFor="postal">Postal Code</label>
-        <input minLength={5} maxLength={5} type="text" id="postal" ref={postalCodeInputRef} />
+        <input minLength="5" maxLength="5" type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode
+        && <p>Please enter a valid postal code (5 characters long)!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={nameControlClasses({ id: 'city' })}>
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
+        {!formInputsValidity.city && <p>Please enter a valid city!</p>}
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={onCancel}>
           Cancel
         </button>
-        {/* eslint-disable-next-line react/button-has-type */}
-        <button className={classes.submit}>Confirm</button>
+        <button className={classes.submit} type="submit">Confirm</button>
       </div>
     </form>
   );
